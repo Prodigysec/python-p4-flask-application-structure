7c0aeb30e5493814300c496743a2a7ca99425f4a
Ian Raburu <williamraburu@gmail.com>
2023-09-18 11:18:07 +0300

    Build flask application

--- server/app.py
+++ server/app.py
@@ -3,3 +3,17 @@
 from flask import Flask
 
 app = Flask(__name__)
+
+# define a route -> associate a url with code to be executed
+@app.route('/')  # register  index() to app instance
+def index():
+    return '<h1>Welcome to my app!</h1>'
+
+@app.route('/<string:username>')
+def user(username):
+    return f'<h1>Profile for {username}</h1>'
+
+if __name__ == '__main__':
+    app.run(port=5555)
+
+# Registration -> connecting a view to app instance
